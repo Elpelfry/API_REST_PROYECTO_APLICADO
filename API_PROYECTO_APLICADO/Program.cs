0e473f5bdbcb44b58762377ad5e77241b69b4e34
Delfry Paulino Ortega <delfry_paulino@ucne.edu.do>
2024-06-18 22:29:41 -0400

    Nuevo

--- API_PROYECTO_APLICADO/Program.cs
+++ API_PROYECTO_APLICADO/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
+ 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
